1c5f11752068ce2576b55d594b68e4ffcf4e68b1
Vincent Ollivier <contact@vincentollivier.com>
2012-02-03 22:53:31 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='Memorious',
-    version='0.0.1',
+    version='0.0.2',
     description='A simple CLI to manage website accounts',
     author='Vincent Ollivier',
     author_email='contact@vincentollivier.com',
     url='https://github.com/vinc/memorious',
     packages=['memorious'],
-    scripts=['scripts/memorious']
+    scripts=['scripts/memorious'],
+    license='GNU GPL v3',
+    install_requires=['pycrypto']
 )
