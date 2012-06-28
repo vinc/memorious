928a11f076aad14ed261cf11b2287d9919788e2f
Vincent Ollivier <contact@vincentollivier.com>
2012-06-28 08:36:50 +0200

    Fix indentations

--- memorious/store.py
+++ memorious/store.py
@@ -27,155 +27,155 @@ from Crypto.Cipher import AES as Cipher
 
 class Store(object):
     """Manage an encrypted passwords database."""
-    
+
     def __init__(self, encrypted_dump_file, key_file, key_size=256):
-	"""Restore in memory database from encrypted dump file.
+        """Restore in memory database from encrypted dump file.
 
-	Keyword arguments:
-	encrypted_dump_file -- the encrypted dump file path
-	key_file -- the key file path
-	key_size -- the key size (128, 192 or 256 bits for AES)
-	"""
-	self.closed = False
+        Keyword arguments:
+        encrypted_dump_file -- the encrypted dump file path
+        key_file -- the key file path
+        key_size -- the key size (128, 192 or 256 bits for AES)
+        """
+        self.closed = False
 
-	# Create a database in memory
-	self._con = sqlite3.connect(':memory:')
+        # Create a database in memory
+        self._con = sqlite3.connect(':memory:')
 
-	# Decrypt database dump file
-	dump = ''
-	self._key = KeyFile(key_file, key_size).key
-	self._path = encrypted_dump_file
-	if os.path.isfile(self._path):
-	    with open(self._path) as f:
-		# Need a block cipher encryption algorithm
-		# operating in cipher feedback (CFB) mode
-		cipher = Cipher.new(self._key, Cipher.MODE_CFB)
-		c = f.read(cipher.block_size)
-		while c != '':
-		    dump = '%s%s' % (dump, cipher.decrypt(c))
-		    c = f.read(cipher.block_size)
-		    cipher = Cipher.new(self._key, Cipher.MODE_CFB, cipher.IV)
-	    # Restore previous database from dump
-	    self._con.executescript(dump)
-	else:
-	    print 'Creating a new database ...'
-	    self._con.execute("""
-		CREATE TABLE slots(
-		    id INTEGER PRIMARY KEY,
-		    domain,
-		    username,
-		    password,
-		    comment
-		);
-		""")
-	
+        # Decrypt database dump file
+        dump = ''
+        self._key = KeyFile(key_file, key_size).key
+        self._path = encrypted_dump_file
+        if os.path.isfile(self._path):
+            with open(self._path) as f:
+                # Need a block cipher encryption algorithm
+                # operating in cipher feedback (CFB) mode
+                cipher = Cipher.new(self._key, Cipher.MODE_CFB)
+                c = f.read(cipher.block_size)
+                while c != '':
+                    dump = '%s%s' % (dump, cipher.decrypt(c))
+                    c = f.read(cipher.block_size)
+                    cipher = Cipher.new(self._key, Cipher.MODE_CFB, cipher.IV)
+            # Restore previous database from dump
+            self._con.executescript(dump)
+        else:
+            print 'Creating a new database ...'
+            self._con.execute("""
+                CREATE TABLE slots(
+                    id INTEGER PRIMARY KEY,
+                    domain,
+                    username,
+                    password,
+                    comment
+                );
+                """)
+
     @classmethod
     def open(cls, encrypted_dump_file, key_file, key_size=256):
-	safe = cls(encrypted_dump_file, key_file, key_size)
-	return safe
-    
+        safe = cls(encrypted_dump_file, key_file, key_size)
+        return safe
+
     def get(self, **params):
-	"""Generate row matching params in the password list."""
-	self._con.row_factory = sqlite3.Row
-	query = 'SELECT * FROM slots'
-	values = [val for val in params.values() if val != '']
-	if values:
-	    query += ' WHERE %s' % ' AND '.join(
-		'%s=?' % key for key in params.keys() if params[key] != ''
-	    )
-	for row in self._con.execute(query, values):
-	    yield row
+        """Generate row matching params in the password list."""
+        self._con.row_factory = sqlite3.Row
+        query = 'SELECT * FROM slots'
+        values = [val for val in params.values() if val != '']
+        if values:
+            query += ' WHERE %s' % ' AND '.join(
+                '%s=?' % key for key in params.keys() if params[key] != ''
+            )
+        for row in self._con.execute(query, values):
+            yield row
 
     def put(self, **params):
-	"""Add a new row in the password list."""
-	assert ''.join(params.keys()).isalpha()
-	q_cols = ', '.join(params.keys())
-	q_vals = ', '.join('?' * len(params))
-	query = "INSERT INTO slots (%s) values (%s)" % (q_cols, q_vals)
-	self._con.execute(query, params.values())
+        """Add a new row in the password list."""
+        assert ''.join(params.keys()).isalpha()
+        q_cols = ', '.join(params.keys())
+        q_vals = ', '.join('?' * len(params))
+        query = "INSERT INTO slots (%s) values (%s)" % (q_cols, q_vals)
+        self._con.execute(query, params.values())
 
     def delete(self, id):
-	"""Remove a row from the password list."""
-	self._con.execute('DELETE FROM slots WHERE id=?', (id,))
+        """Remove a row from the password list."""
+        self._con.execute('DELETE FROM slots WHERE id=?', (id,))
 
     def close(self):
-	if self.closed:
-	    return
-	
-	# Dump database
-	dump = ''.join(self._con.iterdump())
-	
-	# Encrypt dump to the persistent file
-	with open(self._path, 'wb') as f:
-	    cipher = Cipher.new(self._key, Cipher.MODE_CFB)
-	    while dump:
-		f.write(cipher.encrypt(dump[:cipher.block_size]))
-		dump = dump[cipher.block_size:]
-		cipher = Cipher.new(self._key, Cipher.MODE_CFB, cipher.IV)
-	
-	self.closed = True
+        if self.closed:
+            return
+
+        # Dump database
+        dump = ''.join(self._con.iterdump())
+
+        # Encrypt dump to the persistent file
+        with open(self._path, 'wb') as f:
+            cipher = Cipher.new(self._key, Cipher.MODE_CFB)
+            while dump:
+                f.write(cipher.encrypt(dump[:cipher.block_size]))
+                dump = dump[cipher.block_size:]
+                cipher = Cipher.new(self._key, Cipher.MODE_CFB, cipher.IV)
+
+        self.closed = True
 
     def __enter__(self):
-	return self
+        return self
 
     def __exit__(self, type, value, traceback):
-	if type is None:
-	    self.close()
-	else:
-	    # An exception occurred. Any changes in the database should be
-	    # considered lost and not dumped into the encrypted file.
-	    self.closed = True
+        if type is None:
+            self.close()
+        else:
+            # An exception occurred. Any changes in the database should be
+            # considered lost and not dumped into the encrypted file.
+            self.closed = True
 
 
 class KeyFile(object):
 
     def __init__(self, path, key_size=256):
-	self.path = path
-	self.size = key_size
+        self.path = path
+        self.size = key_size
 
     @property
     def key(self):
-	try:
-	    with open(self.path) as f:
-		return f.read(self.size / 8)
-	except IOError:
-	    raise IOError, (errno.ENOENT, "No such key file: '%s'" % self.path)
-    
+        try:
+            with open(self.path) as f:
+                return f.read(self.size / 8)
+        except IOError:
+            raise IOError, (errno.ENOENT, "No such key file: '%s'" % self.path)
+
     @classmethod
     def generate(cls, path, key_size=256, file_size=1024):
-	"""Generate a new key file."""
-	assert file_size >= key_size
-	if os.path.isfile(path):
-	    raise IOError, (errno.EEXIST, "Key file exist: '%s'" % path)
-	with open(path, 'wb') as f:
-	    f.write(os.urandom(file_size))
-	return cls(path, key_size)
+        """Generate a new key file."""
+        assert file_size >= key_size
+        if os.path.isfile(path):
+            raise IOError, (errno.EEXIST, "Key file exist: '%s'" % path)
+        with open(path, 'wb') as f:
+            f.write(os.urandom(file_size))
+        return cls(path, key_size)
 
 class Password(object):
     def __init__(self, password):
-	self.password = password
+        self.password = password
 
     def __repr__(self):
-	return self.password
+        return self.password
 
     def is_strong(self):
-	raise NotImplementedError
+        raise NotImplementedError
 
     @classmethod
     def generate(cls, length, secure):
-	alphabet = string.letters + string.digits
-	if secure:
-	    alphabet += string.punctuation
-	
-	# Uses the os.urandom() function for random numbers generator source
-	rand = SystemRandom()
+        alphabet = string.letters + string.digits
+        if secure:
+            alphabet += string.punctuation
 
-	# Shuffle the alphabet a random number of times
-	perm_list = list(alphabet)
-	for i in xrange(rand.randint(128, 256)):
-	    rand.shuffle(perm_list)
-	
-	# Randomly choose the password in this alphabet
-	password = ''.join(rand.sample(perm_list, length))
+        # Uses the os.urandom() function for random numbers generator source
+        rand = SystemRandom()
 
-	return cls(password)
+        # Shuffle the alphabet a random number of times
+        perm_list = list(alphabet)
+        for i in xrange(rand.randint(128, 256)):
+            rand.shuffle(perm_list)
+
+        # Randomly choose the password in this alphabet
+        password = ''.join(rand.sample(perm_list, length))
+
+        return cls(password)
